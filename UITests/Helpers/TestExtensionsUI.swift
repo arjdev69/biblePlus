2184eecaa1b66b89986ba3f801e45c2a7236e137
ArjDev69 <bruno96araujo@gmail.com>
2021-12-21 22:10:44 -0300

    test: create some components

---
+++ UITests/Helpers/TestExtensionsUI.swift
@@ -0,0 +1,23 @@
+//
+//  TestExtensionsUI.swift
+//  UITests
+//
+//  Created by bruno araujo on 05/08/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach { action in
+                (target as NSObject).perform(Selector(action))
+            }
+        }
+    }
+    
+    func simulateTap() {
+        simulate(event: .touchUpInside)
+    }
+}
